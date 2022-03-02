4294632ff71f81b5f20e0222124c9298944da791
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-02 17:34:01 -0500

    Add a simple toggle to the draw button.

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,27 +5,34 @@ import logo from "./logo.svg";
 //import logo from "./disgamote-logo-white.png";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo"></img>
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <ul className="social-icons">
           {social.map((item) => {
             const { id, url, icon } = item;
